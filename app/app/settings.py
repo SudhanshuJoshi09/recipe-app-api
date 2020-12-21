a3ccfedb213a99881e19abd27f6419666e4b6bab
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-21 13:01:43 +0530

    error-check 7

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*jj(=h&y@!4o0w55tbgsyci#7*e(iag87@pj+xoy+u-58fbhhp'
+SECRET_KEY = '%5r%rlp0&iv+s5+p&o@v26d_#&=n_c^a6yr$wo^+85haj^)d@('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
