ea148b87295951f81274a10d204f385b951207e0
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-21 19:15:48 +0530

    adding sample test cases

--- app/app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-
-class CalcTests(TestCase):
-
-    def test_add_numbers(self):
-        """ Test that two numbers add together """
-        self.assertEqual(add(8, 3), 11)
