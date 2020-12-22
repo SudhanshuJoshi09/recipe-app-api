8fee5ed9c05def35fb404063ad6490edd004cc4b
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-22 22:34:30 +0530

    few touch ups

---
+++ app/user/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserSerializers(serializers.ModelSerializer):
+    """Serializers for the Users objects"""
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializers for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
