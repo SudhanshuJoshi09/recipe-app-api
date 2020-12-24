f76069cb763d631bfb6dd59bdf00617f52c75c0c
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-24 12:48:39 +0530

    adding mangeuserview

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _
 from core import models
 
 
@@ -9,12 +9,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (('Personal Info'), {'fields': ('name',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-            ('Permissions'),
+            _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (('Important Dates'), {'fields': ('last_login',)})
+        (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
--- app/user/serializers.py
+++ app/user/serializers.py
@@ -15,6 +15,17 @@ class UserSerializers(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializers for the user authentication object"""
@@ -28,3 +39,14 @@ class AuthTokenSerializer(serializers.Serializer):
         """Validate and authenticate user"""
         email = attrs.get('email')
         password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+        attrs['user'] = user
+        return attrs
